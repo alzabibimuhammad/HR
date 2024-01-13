c8315823729b7a920a604b65ca6352e8f7dc4e67
Abood-khd <112219742+Abood-khd@users.noreply.github.com>
2024-01-13 20:03:51 +0300

    fix icon delete in report component

--- src/features/Report/componets/dialog/index.jsx
+++ src/features/Report/componets/dialog/index.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Typography } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { RemoveReport, getReportsData } from "src/pages/report/store";
+import { Box, Stack } from '@mui/system'
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
 
 export default function AlertDialog({ id ,open ,handleClose , handleDelete }) {
   const [localId, setLocalId] = useState(id);
@@ -14,30 +18,51 @@ export default function AlertDialog({ id ,open ,handleClose , handleDelete }) {
       handleClose()
   };
 
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+
   return (
 
-    <React.Fragment>
+    <>
+      <Box >
+      <Grid columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
       <Dialog
         onClose={handleClose}
         open={open}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
         >
-        <DialogTitle style={{ fontSize: "19px", color: '#B4B4B3' }}>
-          {"Are you sure you want to delete report?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            <ErrorOutlineIcon style={{ color: '#A20D29', marginLeft: '80px', fontSize: 140 }} />
+        <Grid item xs={12}>
+          <Item>
+          <DialogContent sx={{height:"99px",width:"100%",borderRadius:"10px",position:"relative",overflow:"visible"}}>
+          <DialogContentText sx={{}}>
+            <CancelRoundedIcon  style={{ backgroundColor:"#FFFFFF",color: '#A20D29' ,fontSize: 160,position:"absolute",top: "50%", left: "50%", transform: "translate(-50%, -90%)",borderRadius:"50%" }} />
+
           </DialogContentText>
         </DialogContent>
-        <DialogActions style={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
+        <Typography  sx={{fontWeight:"600",fontSize:"16px",color:"#131627"}}>Delete</Typography>
+
+
+          <DialogTitle style={{ fontSize: "19px", color: '#B4B4B3' }}>
+          {"Are you sure you want to delete report?"}
+        </DialogTitle>
+
+
+          <DialogActions style={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
           <Button onClick={handleClose} style={{ color: '#B4B4B3' }}>Cancel</Button>
-          <Button onClick={()=>handleDeleteAPI()} autoFocus>
+          <Button  sx={{color:"#DF2E38"}}  onClick={()=>handleDeleteAPI()} autoFocus>
             Delete
           </Button>
         </DialogActions>
+        </Item>
+        </Grid>
       </Dialog>
-    </React.Fragment>
+      </Grid>
+    </Box>
+    </>
   );
 }

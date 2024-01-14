708bf747ab609271250006e96ec446d8b1e2f505
Abood-khd <112219742+Abood-khd@users.noreply.github.com>
2024-01-14 13:39:32 +0300

    fix view pdf

--- src/features/Contracts/list/Componets/Profile/componets/PdfViwer.jsx
+++ src/features/Contracts/list/Componets/Profile/componets/PdfViwer.jsx
@@ -1,15 +1,18 @@
 import { Worker, Viewer } from '@react-pdf-viewer/core';
+import {defaultLayoutPlugin} from "@react-pdf-viewer/default-layout"
 import '@react-pdf-viewer/core/lib/styles/index.css';
-import { pdfjs } from 'react-pdf';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import { Box, Stack } from '@mui/system'
 
 const PDFViewer = ({ pdfUrl }) => {
+    const newplugin = defaultLayoutPlugin()
 
   return (
-    <div>
-      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`}>
-        <Viewer fileUrl={pdfUrl} />
+    <Box sx={{height:"365px",width:"99%.5",marginLeft:"1px"}}>
+      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.min.js`}>
+        <Viewer  fileUrl={pdfUrl} plugins={[newplugin]} />
       </Worker>
-    </div>
+    </Box>
   );
 };
 

dfc784ee065278c430061f46715ba5d92f4e2a6e
alzabibimuhammad <alzabibimuhammad@gmail.com>
2024-01-31 12:05:43 +0300

    tree card

--- src/pages/tree/index.jsx
+++ src/pages/tree/index.jsx
@@ -1,6 +1,7 @@
-import { Stack } from '@mui/system'
+import { Box, Stack } from '@mui/system'
 import dynamic from 'next/dynamic';
 import React from 'react'
+import Logo from 'src/features/Contracts/view/logo';
 import useGetMembersHierarchy from 'src/features/tree/hooks/useGetMembersHierarchy';
 
 
@@ -16,9 +17,14 @@ export default function Tree() {
 
 
   return (
+      <Stack spacing={3}>
+      <Box size={'small'} >
+        <Logo />
+      </Box>
 
       <Stack height={'100%'} >
         <TreeComponent/>
+      </Stack>
       </Stack>
   )
 }

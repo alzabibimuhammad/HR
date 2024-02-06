23dd8e691077235d753a6672f9c3b41407af3ef6
Abood-khd <112219742+Abood-khd@users.noreply.github.com>
2024-02-06 16:48:13 +0300

    first work

---
+++ src/features/profile/componets/manage/hook/useDeleteDecision.js
@@ -0,0 +1,13 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import DeleteDecision from "../api/DeleteDecision";
+
+export const useDeleteDecision = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn:DeleteDecision,
+    onSuccess: () => {
+      queryClient.invalidateQueries("DeleteDecision");
+    },
+  });
+};
--- src/features/profile/ratingTabel/api/GetRatingById.js
+++ src/features/profile/ratingTabel/api/GetRatingById.js
@@ -1,7 +1,7 @@
 import { request } from "../../../../utiltis/AxiosUtilitis";
 
 const getRatingById = async () => {
-  return request({ url: '/api/Rate/getRate/11?date=2024-02-05' })
+  // return request({ url: '/api/Rate/getRate/11?date=2024-02-05' })
 
 };
 

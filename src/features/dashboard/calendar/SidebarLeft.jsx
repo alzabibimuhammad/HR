1f8fa6cb376b6e896e84ff1f952f86ba7da57150
alzabibimuhammad <alzabibimuhammad@gmail.com>
2024-02-13 13:14:33 +0300

    fix style event

--- src/features/dashboard/calendar/SidebarLeft.jsx
+++ src/features/dashboard/calendar/SidebarLeft.jsx
@@ -9,6 +9,8 @@ import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import { useTranslation } from 'react-i18next';
 import Image from 'next/image';
+import { Stack } from '@mui/system';
+import { Divider } from '@mui/material';
 
 const steps = [
   {
@@ -45,29 +47,65 @@ export default function SidebarLeft({DataEventByDay}) {
   };
 
   return (
-    <Box sx={{ backgroundColor:"#E9ECF3" }}>
-       {Array.isArray(DataEventByDay?.data?.data) && DataEventByDay.data.data.length > 0 ? (
-      DataEventByDay.data.data.map(event => (
-        <div className="parent" key={event.id}>
-          <div>
-            <p><span className="child">{event.day}</span>{event.start}</p>
-            {/* <p>{event.time}</p> */}
-          </div>
-          <div>
-            <p className='description'>
-              {event.description}
-            </p>
-          </div>
-        </div>
-      ))
-    ) : (
-      <Image
-        width={250}
-        height={250}
-        src="/images/notRequest.svg"
-        alt="Alternate Text"
-      />
-    )}
-    </Box>
+    <Stack width={'300px'} height={'300px'} overflow={'scroll'} sx={{'&::-webkit-scrollbar': {width: '3px' },}} spacing={1} direction={'column'} >
+    {Array.isArray(DataEventByDay?.data?.data) && DataEventByDay.data.data.length > 0 ? (
+   DataEventByDay.data.data.map(event => (
+     <>
+       <Stack
+               direction={'row'}
+               justifyContent={'start'}
+               sx={{ padding: 0, margin: 0, width: '100%', height: '160px'}}
+               className='parent'
+               key={event.id}
+             >
+
+               <Stack alignItems={'center'} direction={'column'}>
+
+                 <span style={{ padding: 0, margin: 0 }} className='child'>
+                   {event.day}
+                 </span>
+
+                 <Typography width={'54px'} p={0} ml={0} variant='p' fontSize={10}>
+                   {event.start}
+                 </Typography>
+               </Stack>
+
+               <Box
+                 sx={{
+                   width: '160px',
+                   height: '65px',
+                   textAlign: 'start',
+                   alignItems:'center',
+                   lineHeight: '15px',
+                   overflowY: 'auto',
+                   overflowWrap: 'break-word',
+                   wordWrap: 'break-word',
+                   '&::-webkit-scrollbar': {
+                     width: '0px' /* This will hide the scrollbar on WebKit browsers (e.g., Chrome, Safari) */
+                   },
+                   scrollbarWidth: 'none' /* This will hide the scrollbar on Firefox */
+                 }}
+               >
+                 <Typography variant='p' fontSize={12} color={'#000'} style={{ padding: 0, margin: 0 }}>
+                   {event.description}
+                 </Typography>
+               </Box>
+             </Stack>
+       </>
+
+   ))
+
+ ) : (
+   <Box width={'100%'}>
+   <img
+     style={{ width:'100%',height:'100%' }}
+
+     src="/images/notRequest.svg"
+     alt="Alternate Text"
+   />
+   </Box>
+ )}
+   </Stack>
+
   );
 }
